b59ef1aa4c38f753a56e5c228032a0bfc4eb47a1
Fabricyo Ribeiro <69694301+fabricyoribeiro@users.noreply.github.com>
2022-06-18 22:32:15 -0300

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,10 @@
 const express = require('express')
 const app = express()
 
+var cors = require('cors')
+
+app.use(cors())
+
 app.get('/produtos', (req, res) => {
   res.json({ nome: 'Notebook', preco: 1200})
 })
